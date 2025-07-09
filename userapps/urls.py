61558013236406423b6a6e6525c723dcc0d9d982
Nathaniel Nosa <nathanielnarth@gmail.com>
2025-07-09 13:11:02 +0100

    Registration view and serializer with Url

--- userapps/urls.py
+++ userapps/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    
+    path('register/', views.RegistrationView.as_view())
 ]
--- userapps/views.py
+++ userapps/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status, serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+from . serializers import UserSerializer, ProfileSerializer, RegistrationSerializer
+from . models import Profile
+
+class RegistrationView(APIView):
+    def post(self,request):
+        try:
+            serializers = RegistrationSerializer(data = request.data)
+            if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data, status=status.HTTP_201_CREATED)
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"Error":str(e)})
+        
