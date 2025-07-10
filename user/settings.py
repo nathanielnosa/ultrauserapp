4210cc79fe511433b3e3e8ce4f2ace7d462a9c91
Nathaniel Nosa <nathanielnarth@gmail.com>
2025-07-10 11:47:21 +0100

    baseurl to allowed host

--- user/settings.py
+++ user/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ultrauserapp.onrender.com']
 
 
 # Application definition
