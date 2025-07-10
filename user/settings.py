724b5636ef264d791250714265bf241ab50cd7c3
Nathaniel Nosa <nathanielnarth@gmail.com>
2025-07-10 11:53:04 +0100

    third deploy

--- user/settings.py
+++ user/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://ultrauserapp.onrender.com']
+ALLOWED_HOSTS = ['https://ultrauserapp.onrender.com','ultrauserapp.onrender.com','http://ultrauserapp.onrender.com']
 
 
 # Application definition
